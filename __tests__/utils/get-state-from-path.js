b523c76ddfe1cdbf6a934a5be59ee661c8d24243
Conor Hastings <hastings.conorm@gmail.com>
2018-02-05 06:01:30 -0500

    refactor to act as if api is external to application

--- __tests__/utils/get-state-from-path.js
+++ __tests__/utils/get-state-from-path.js
@@ -9,19 +9,15 @@ const history = {
 }
 
 test('should correctly return state for given path', () => {
-  const launches = getLaunchDates({ year: 2017, month: 1 });
-  fetch.mockResponse(JSON.stringify(launches));
-  return getStateFromPath(history).then(state => {
-    /* differing system locales cause this value to mess up so removing from test */
-    state.monthName = undefined;
-    expect(state).toEqual({
-      loading: false,
-      year: 2017, 
-      month: 1, 
-      firstWeekDay: 0,
-      daysInMonth: 31,
-      weekRows: getWeekArrays({ daysInMonth: 31, firstWeekDay: 0 }),
-      launches: launches
-    });
+  const state =  getStateFromPath(history);
+  /* differing system locales cause this value to mess up so removing from test */
+  state.monthName = undefined;
+  expect(state).toEqual({
+    loading: false,
+    year: 2017, 
+    month: 1, 
+    firstWeekDay: 0,
+    daysInMonth: 31,
+    weekRows: getWeekArrays({ daysInMonth: 31, firstWeekDay: 0 })
   });
 });
